39eaa99a51b6f65528a5eeb8cf06337f92cf36d3
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-01-15 23:18:06 -0300

    primer commit logica Alura

---
+++ logica-1-Aula1/app.js
@@ -0,0 +1,9 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas el número por favor"); //Variable
+
+alert ("Hello World");
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto){ //Un ()=) es asignacion (==) es comparación
+ alert("Acertaste")
+}
