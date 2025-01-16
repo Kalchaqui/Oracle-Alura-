68b0072849e182f5546bb1899afcc86d587456f2
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-01-16 02:22:13 -0300

    2DO commit logica Alura

--- logica-1-Aula1/app.js
+++ logica-1-Aula1/app.js
@@ -1,9 +1,15 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas el número por favor"); //Variable
-
+let numeroUsuario = prompt("Me indicas el número del 1 al 10 por favor"); //Variable
+/*
+Comentado de 
+Código
+*/
 alert ("Hello World");
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto){ //Un ()=) es asignacion (==) es comparación
- alert("Acertaste")
+ alert(`Acertaste, el numero es: ${numeroSecreto}`);// aca le estoy avisando a JS que me tire una variable `xx ${} ` 
+}else {
+    alert("Lo siento no acertaste el número");
 }
--- logica-1-Aula2/app.js
+++ logica-1-Aula2/app.js
@@ -14,3 +14,39 @@ if (numeroUsuario == numeroSecreto) {
     //La condición no se cumplió
     alert('Lo siento, no acertaste el número');
 }
+
+/*
+TAREA
+//1.-Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+let diaSemana = prompt('¿Qué día de la semana es hoy?');
+
+if (diaSemana == 'Sábado' || diaSemana == 'Domingo'){
+    alert('¡Buen fin de semana!');
+} else {
+    alert('¡Buena semana!');
+}
+//2.-Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+let numero = prompt('Ingrese un número');
+if (numero > 0){
+    alert('El numero es positivo')
+}else if (numero < 0){
+    alert('El numero es negativo');
+}
+else{
+    alert('El numero es 0');
+}
+//3.Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
+let puntuacion = prompt('Ingrese su puntuación');
+if (puntuacion>=100){
+    alert('¡Felicidades, has ganado!');
+}else{
+    alert('Intentalo nuevamente para ganar.');
+}
+//4.-Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+let  saldo = prompt('Ingrese su saldo');
+alert(`Tu saldo es ${saldo}`);
+//5.-Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+let nombre = prompt('Ingrese su nombre');
+alert(`¡Bienvenido ${nombre}!`);
+
+*/
