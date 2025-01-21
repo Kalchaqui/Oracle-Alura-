e096ba809c4ac1e87339b85240e13669616a2fd9
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-01-21 15:33:31 -0300

    4to commit logica Alura

--- logica-1-Aula3/app.js
+++ logica-1-Aula3/app.js
@@ -1,6 +1,14 @@
 //LOOPS Y TENTATIVAS
 
 let numeroSecreto = 2;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
+//Creamos el bucle While, en este caso quda todo el proceso adentro-Mientras el número sea diferente se repite el bucle si gano no.
+//Gerarquia, el bloque que esta dentro del while es un hijo.
+while(numeroUsuario != numeroSecreto){ 
+
+
 let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
@@ -10,7 +18,7 @@ la comparación
 */
 if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
+    alert(`Acertaste, el número es: ${numeroUsuario} Lo hiciste en: ${intentos} ${palabraVeces}`);
 } else { //Condicionales anidados
     if(numeroUsuario > numeroSecreto){
         alert(`El numero secreto es menor que:  ${numeroUsuario}`)
@@ -20,4 +28,6 @@ if (numeroUsuario == numeroSecreto) {
     //La condición no se cumplió
     //alert('Lo siento, no acertaste el número');
 }
-
+intentos = intentos + 1; //tentativa o intento, incremento cuando no acierta
+palabraVeces = "veces";
+}
